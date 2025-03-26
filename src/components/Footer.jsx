4e15f77d6d9fcd449f400d299266916367439863
tiagocuzzuolnunes <tiagocuzzuolnunes@gmail.com>
2025-03-26 07:40:24 -0300

    commiting changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import MainButton from "./MainButton";
+import SocialLogo from "./SocialLogo";
 
 // export default function Footer() {
 //     return (
@@ -15,41 +16,134 @@ import React from "react";
 
 function Footer() {
     return (
-        <div className="bg-primary-darker flex flex-col justify-center items-center p-4">
-            <div>
-                <p className="font-sans text-white font-semibold text-md text-left w-full">
-                    Siga-nos
-                </p>
-                <div className="mt-1 flex flex-row text-left w-full mb-4 gap-1">
-                    <img src="/instagramLogo.svg" alt="Logo Instagram" className="w-8" />
-                    <img src="/youtubeLogo.svg" alt="Logo Youtube" className="w-8" />
-                </div>
+        <section className="bg-primary-darker flex flex-col justify-center items-center p-4">
+
+            <div className="grid grid-cols-8">
+
+                <article className="text-white col-span-2">
+
+                    <h4 className="cursor-default font-semibold text-md">
+                        Destaques
+                    </h4>
+                    <ul>
+
+                        <li>
+                            <span className="cursor-pointer font-light hover:text-neutral-normal-hover">
+                                Mercado
+                            </span>
+                        </li>
+
+                        <li>
+                            <span className="cursor-pointer font-light hover:text-neutral-normal-hover">
+                                Bebidas
+                            </span>
+                        </li>
+
+                        <li>
+                            <span className="cursor-pointer font-light hover:text-neutral-normal-hover">
+                                Farmácia
+                            </span>
+                        </li>
+
+                        <li>
+                            <span className="cursor-pointer font-light hover:text-neutral-normal-hover">
+                                Saudável
+                            </span>
+                        </li>
+
+                        <li>
+                            <span className="cursor-pointer font-light hover:text-neutral-normal-hover">
+                                Padaria
+                            </span>
+                        </li>
+
+                        <li>
+                            <span className="cursor-pointer font-light hover:text-neutral-normal-hover">
+                                Regional
+                            </span>
+                        </li>
+
+                        <li>
+                            <span className="cursor-pointer font-light hover:text-neutral-normal-hover">
+                                Todas as categorias
+                            </span>
+                        </li>
+
+                        <li>
+                            <span className="cursor-pointer font-light hover:text-neutral-normal-hover">
+                                Cupons
+                            </span>
+                        </li>
+
+                        <li>
+                            <span className="cursor-pointer font-light hover:text-neutral-normal-hover">
+                                Cashback
+                            </span>
+                        </li>
+
+                    </ul>
+
+                </article>
+
+                <article className="text-white col-span-2">
+
+                    <h4 className="cursor-default font-semibold text-lg">
+                        Precisa de ajuda?
+                    </h4>
+
+                    <MainButton content="Tire sua Dúvida" />
+
+                </article>
+
+                <article>
+
+                    <p className="font-sans text-white font-semibold text-md text-left w-full">
+                        Siga-nos
+                    </p>
+
+                    <div className="mt-1 flex flex-row text-left w-full mb-4 gap-1">
+
+                        <SocialLogo src="/instagramLogo.svg" alt="Logo Instagram. Clique para entrar no Instagram da Flokia" />
+
+                        <SocialLogo src="/youtubeLogo.svg" alt="Logo Youtube. Clique para entrar no Youtube da Flokia" />
+
+                    </div>
+
+                </article>
+
+                <article>
+                    <p className="font-sans text-white font-semibold text-md text-left w-full">
+                        Baixe nosso aplicativo
+                    </p>
+                    <div className="mt-1 flex flex-row text-left w-full mb-4 gap-1">
+                        <SocialLogo src="/appleLogo.svg" alt="Logo da Apple. Clique para instalação de app IOS" className="w-8" />
+                        <SocialLogo src="/androidLogo.svg" alt="Logo da Android. Clique para instalação de app Android" className="w-8" />
+                    </div>
+                </article>
+
             </div>
-            <div>
-                <p className="font-sans text-white font-semibold text-md text-left w-full">
-                    Baixe nosso aplicativo
-                </p>
-                <div  className="mt-1 flex flex-row text-left w-full mb-4 gap-1">
-                    <img src="/appleLogo.svg" alt="Logo da Apple para instalação de app IOS" className="w-8" />
-                    <img src="/androidLogo.svg" alt="Logo da Android para instalação de app Android" className="w-8" />
-                </div>
-            </div>
-            <span className="font-sans text-white font-medium border-t-1 border-b-1 w-full text-center py-2 text-sm border-gray-400">
+
+            <div className="font-sans text-white font-medium border-t-1 border-b-1 w-full text-center py-2 my-10 text-sm border-gray-400">
                 Compare preços e produtos de grandes marcas.
-            </span>
+            </div>
+
             <span className="font-sans text-white text-sm text-left w-full mt-6">
                 O uso deste site está sujeito aos termos e condições do <a href="https://github.com/Flokkia" target="_blank" className="underline">Termo de Uso</a> e <a href="https://github.com/Flokkia" target="_blank" className="underline">Política de privacidade</a>.
             </span>
+
             <span className="font-sans text-white text-sm text-left w-full mt-5">
                 CNPJ: 097738538593 / Rua Capote, 1º andar - São Paulo - SP
             </span>
+
             <p className="font-sans text-white text-xs mt-8 mb-4">
                 © Copyrights 2024 - Todos os direitos reservados.
             </p>
+
             <div className="my-2">
                 <img src="/FlokiaLogo2.svg" alt="Logo da Flokia" className="max-w-[15vw]" />
             </div>
-        </div>
+
+        </section>
     );
 }
 
--- src/components/SocialLogo.jsx
+++ src/components/SocialLogo.jsx
@@ -1,7 +1,7 @@
 export default function SocialLogo({ src, alt }) {
     return (
-        <button className="border-box w-8 bg-white rounded-md flex justify-center items-center">
-            <img src={src} alt={alt} className="object-contain"/>
+        <button className="border-box w-8 h-8 p-1 bg-white rounded-md flex justify-center items-center">
+            <img src={src} alt={alt} className="object-cover"/>
         </button>
     )
 }
